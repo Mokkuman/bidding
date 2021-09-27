5cfc14e0ffdd12e366e001c0b9dcebd0bf7e12cd
Rami1234Sh <guamo45@gmail.com>
2021-09-27 17:03:59 -0500

    Creating Product and Person models

--- store/models.py
+++ store/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    IDProduct = models.IntegerField()
+    productName = models.CharField(max_length=50)
+    price = models.FloatField()
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.productName
+    
+    def getID(self):
+        return self.IDProduct
+
+class Person(models.Manager):
+    IDPerson = models.IntegerField(max_length=50)
+    firstName = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.firstName + self.lastName
+    
+    def getID(self):
+        return self.IDPerson
+
