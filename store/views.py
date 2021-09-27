1c2686d50048365986738df052c8c6d680030d30
Danny <65949272+DannyMannn@users.noreply.github.com>
2021-09-27 17:34:34 -0500

    Adds product page
    - Product page template and stylesheet
    -  URL path for products (at the moment it's static).
    - New view function
    - Adds cssPath block for inheritance

--- store/views.py
+++ store/views.py
@@ -5,3 +5,21 @@ from django.http import HttpResponse
 def index(request):
     #return HttpResponse("Hola pvtas")
     return render(request,"store/index.html")
+
+def goToProduct(request):
+    try:
+        dummyData = {
+            'name':'Product 1XX',
+            'currentBid':100,
+            'description':'Very cool treasure box',
+            'category':'TOYS',
+            'condition':'LIKE NEW'
+        }
+        return render(request, 'store/productTemplate.html',{
+            'product':dummyData,
+            'productFound':True
+        })
+    except Exception as exception:
+        return render(request, "store/productTemplate.html",{
+            'productFound':False
+        })
