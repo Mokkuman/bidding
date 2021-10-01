4945ed1e1ad25b890a499235f125df05a90ad134
Mokkuman <droseher@gmail.com>
2021-10-01 18:40:25 -0500

    Actualizando models.py y agregando la app 'users' y ya

--- bidding/settings.py
+++ bidding/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'store', #app creada
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- store/models.py
+++ store/models.py
@@ -3,20 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    IDProduct = models.IntegerField()
-    productName = models.CharField(max_length=50)
-    price = models.FloatField()
-    description = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.productName
-    
-    def getID(self):
-        return self.IDProduct
-
-
-class Product(models.Model):
-    IDProduct = models.IntegerField(max_length=50)
+    #IDProduct = models.IntegerField()
     productName = models.CharField(max_length=50)
     price = models.FloatField(max_length=50)
     description = models.CharField(max_length=200)
@@ -28,8 +15,8 @@ class Product(models.Model):
     def __str__(self):
         return self.productName
     
-    def getID(self):
-        return self.IDProduct
+    # def getID(self):
+    #     return self.IDProduct
 
     def getPrice(self):
         return self.price
@@ -50,7 +37,7 @@ class Product(models.Model):
         return self.condition
 
 class User(models.Model):
-    IDUser = models.IntegerField(max_length=50)
+   # IDUser = models.IntegerField()
     firstName = models.CharField(max_length=100)
     lastName = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
@@ -60,8 +47,8 @@ class User(models.Model):
     def __str__(self):
         return self.firstName + self.lastName
     
-    def getID(self):
-        return self.IDUser
+    # def getID(self):
+    #     return self.IDUser
 
     def getEmail(self):
         return self.email
@@ -72,13 +59,13 @@ class User(models.Model):
     def getAdress(self):
         return self.adress
 
-class Admin(models.Manager):
-    name = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
+# class Admin(models.Manager):
+#     name = models.CharField(max_length=50)
+#     password = models.CharField(max_length=50)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
     
-    def getPassword(self):
-        return self.password
+#     def getPassword(self):
+#         return self.password
 
