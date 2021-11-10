7d89f6bb84f3924c45dbd082a9d13f5ae3309d9d
Rami1234Sh <guamo45@gmail.com>
2021-11-09 22:32:59 -0600

    Visualización de productos en index

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from store.models import StockProduct,BidProduct
 
 # Create your views here.
 def index(request):
@@ -6,3 +7,10 @@ def index(request):
 
 def FAQ(request):
     return render(request,"core/FAQ.html")
+
+def index(request):
+	context = {
+        'stockProducts':StockProduct.objects.all(),
+        'bidProducts':BidProduct.objects.all()
+    }
+	return render(request,'core/index.html',context)
