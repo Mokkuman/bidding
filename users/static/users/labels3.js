49dcd21557690def451c46a8c3cd1cde84cfeeb0
Rami1234Sh <guamo45@gmail.com>
2021-11-29 00:13:50 -0600

    -decorando checkoutConfirmation -Traduciendo la seccion updateProfile

---
+++ users/static/users/labels3.js
@@ -0,0 +1,13 @@
+function myGeeks() {
+    var x1 = document.querySelector("label[for='id_firstName'");
+    var x2 = document.querySelector("label[for='id_lastName'");
+    var x3 = document.querySelector("label[for='id_city'");
+    var x4 = document.querySelector("label[for='id_state'");
+    var x5 = document.querySelector("label[for='id_profilePic");
+    x1.innerHTML = "Nombre: ";
+    x2.innerHTML = "Apellido: ";
+    x3.innerHTML = "Ciudad: ";
+    x4.innerHTML = "Estado: ";
+    x5.innerHTML = "Foto de perfil: ";
+}
+myGeeks();
