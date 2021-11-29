6866395ba99261b67c6bab039a61077b47760e58
Rami1234Sh <guamo45@gmail.com>
2021-11-28 22:40:11 -0600

    cambiando cosas de ingles a español (checkout)

---
+++ store/static/store/checkoutLabels.js
@@ -0,0 +1,11 @@
+function myGeeks() {
+    var x1 = document.querySelector("label[for='id_fullName'");
+    var x2 = document.querySelector("label[for='id_address'");
+    var x3 = document.querySelector("label[for='id_city'");
+    var x4 = document.querySelector("label[for='id_phone'");
+    x1.innerHTML = "Nombre Completo: "
+    x2.innerHTML = "Dirección: "
+    x3.innerHTML = "Ciudad: "
+    x4.innerHTML = "Teléfono: "
+}
+myGeeks();
