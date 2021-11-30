32606af48c83beb4ee4324866c329cfb3a01b443
Rami1234Sh <guamo45@gmail.com>
2021-11-29 18:58:50 -0600

    traduciendo mas cosas

---
+++ users/static/users/labels5.js
@@ -0,0 +1,11 @@
+function myGeeks() {
+    var x1 = document.querySelector("label[for='id_productName'");
+    var x2 = document.querySelector("label[for='id_description'");
+    var x3 = document.querySelector("label[for='id_category'");
+    var x6 = document.querySelector("label[for='id_isActive");
+    x1.innerHTML = "Nombre del producto: ";
+    x2.innerHTML = "Descripcion: ";
+    x3.innerHTML = "Categoria: ";
+    x6.innerHTML = "Activo: ";
+}
+myGeeks();
